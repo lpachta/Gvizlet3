68bd3f077ba78178be1295a63a71cc6aa2c5e08d
lpachta <lukin.pachta@gmail.com>
2026-02-28 12:57:22 +0100

    Pruchod workin

---
+++ data.js
@@ -0,0 +1,20 @@
+const mojeKarty = [
+  {
+    frontText: "Pes",
+    frontImg: "https://placedog.net/200/150?random",
+    backText: "Dog",
+    backImg: ""
+  },
+  {
+    frontText: "Kočka",
+    frontImg: "",
+    backText: "Cat",
+    backImg: "https://images.squarespace-cdn.com/content/v1/6542f92b55198b336e629c3e/4f153c07-8d8f-4d81-8eb2-0239ac34590a/AdobeStock_673703733.jpeg?format=2500w"
+  },
+  {
+    frontText: "Hory",
+    frontImg: "https://picsum.photos/id/10/200/150",
+    backText: "Mountains",
+    backImg: "https://picsum.photos/id/1015/200/150"
+  }
+];
